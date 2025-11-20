5ab0c06efe4d493fc49520ae74931bb3f8ae2b94
Alan Burr <10787154+Corpsicle@users.noreply.github.com>
2025-11-20 09:54:31 +0000

    commited 20/11/2025

--- obj/Debug/net9.0/Variables.AssemblyInfo.cs
+++ obj/Debug/net9.0/Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9defb5cfcf61083f20286ac5f0a84c4f95565a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d50db364b740b5436fb5ce04569b0ecc1b5248")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
